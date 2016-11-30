f00efe86c829a7772ba2d87c6a0ed99e00d5d392
Khyatee Desai <Khyatee1@0587372815.wireless.umich.net>
2016-11-29 21:49:34 -0500

    tuesday night- boundaries

--- p4.py
+++ p4.py
@@ -9,7 +9,7 @@ from random import *
 
        
 
-bgcolor = (140, 170,255)    #Color taken from background of sprite
+bgcolor = (40, 40, 60)    #Color taken from background of sprite
 
 class Player(Sprite):
     def __init__(self):
@@ -36,7 +36,7 @@ class Ball(Player):
         Sprite.__init__(self)
         p =pygame.image.load("ball.png")
         self.image = pygame.transform.scale(p, (50,50))
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect(center=(-20,-20))
 
      def hit(self, target):
         return self.rect.colliderect(target)
@@ -51,12 +51,15 @@ display.set_caption('Climb through the Ice')
 # hide the mouse cursor so we only see shovel
 mouse.set_visible(False)
 
+
 f = font.Font(None, 25)
 DELAY = 1000;
-# create the mole and shovel using the constructors
+##### Constructors ######
 player = Player()
 ball = Ball()
 ice = []
+
+screen_rect = screen.get_rect()
 rows = range(300,850,100)
 cols = range(100,650,100)
 for i in rows:
@@ -70,7 +73,7 @@ sprites = RenderPlain(player, ice,ball)
 
 x_pos=30
 y_pos=30
-rep=0
+repeat=True
 hits=0
 time.set_timer(USEREVENT + 1, DELAY)
 # loop until user quits
@@ -83,10 +86,7 @@ while True:
          
     elif e.type == KEYDOWN: 
     
-        # if e.key == K_LEFT:
-        #     x_pos -= 20
-        # if e.key == K_RIGHT:
-        #     x_pos += 20
+      
         if e.key == K_UP:
             y_pos -= 20
         if e.key == K_DOWN:
@@ -99,32 +99,42 @@ while True:
             x = x_pos
             
 
-            while rep != 9:
+            while repeat == True:
                 ball.move(x, y_pos)
                 sprites.update()
                 sprites.draw(screen)
                 pygame.display.update()
                 screen.fill(bgcolor)
-                x+=20 
+                x+=20
+                if x > screen_rect.right:
+                    mixer.Sound("splash.wav").play()
+                    ball.move(-15,-15)
+                    break
+
                 for i in ice:
                     if ball.hit(i): 
-                        rep = 9
+                        repeat = False
+                        i.kill()
+                        ball.move(-15,-15)
+                        i.move(0,0)
+                        mixer.Sound("hit.wav").play()
+                        hits+=1
                         break
-            i.kill()
-            ball.move(0,0)
-            i.move(0,0)
-            mixer.Sound("hit.wav").play()
-            hits+=1
-            rep = 0       
                 
-                        
+                    
+            
+            repeat = True
+            time.set_timer(USEREVENT + 1, DELAY)      
+                
+    # elif e.type == USEREVENT +1:
+
 
 
     # refill background color so that we can paint sprites in new locations
+           # draw text to screen.  Can you move it?
     screen.fill(bgcolor)
-    t = f.render("Score: " + str(hits), False, (0,0,0))
-    screen.blit(t, (320, 0))        # draw text to screen.  Can you move it?
-
+    t = f.render("Score: " + str(hits), False, (255,255,255))
+    screen.blit(t, (320, 0))
     # update and redraw sprites
     sprites.update()
     sprites.draw(screen)
