d28e794cdf1a83324ca0ddf431cb592975be92e5
Khyatee Desai <Khyatee1@0587372815.wireless.umich.net>
2016-12-01 22:35:08 -0500

    Thursday night w/ background

--- p4.py
+++ p4.py
@@ -5,14 +5,14 @@
 from pygame import *
 from pygame.sprite import *
 from random import *
-
+import math
 
 B_WIDTH = 10
 B_HEIGHT = 13
-
+bgcolor = (40, 40, 60) 
        
 
-bgcolor = (40, 40, 60)    #Color taken from background of sprite
+   
 
 class Player(Sprite):
     def __init__(self):
@@ -21,28 +21,9 @@ class Player(Sprite):
         self.image = pygame.transform.scale(p, (75,75))
         self.rect = self.image.get_rect()
 
-    # move gold to a new random location
+  
     def move(self, x_pos, y_pos):
-
-        self.rect.center = (x_pos,y_pos)
-
-    
-class Ice(Player):
-    def __init__(self, row, col):
-        Sprite.__init__(self)
-        ice = image.load('ice.png').convert()
-        self.image = pygame.transform.scale(ice, (80,80))
-        self.rect = self.image.get_rect()
-        self.rect.center = (row, col)
-class Ball(Player):
-     def __init__(self):
-        Sprite.__init__(self)
-        p =pygame.image.load("ball.png")
-        self.image = pygame.transform.scale(p, (40,40))
-        self.rect = self.image.get_rect(center=(-20,-20))
-
-     def hit(self, target):
-        return self.rect.colliderect(target)
+        self.rect.center = (x_pos,y_pos)   
 
 class Gold(Sprite):
     def __init__(self):
@@ -57,16 +38,30 @@ class Gold(Sprite):
         randY = randint(0, 700)
         self.rect.center = (randX,randY)
 
+class Ice(Player):
+    def __init__(self, row, col):
+        Sprite.__init__(self)
+        ice = image.load('ice.png').convert()
+        self.image = pygame.transform.scale(ice, (80,80))
+        self.rect = self.image.get_rect(center=(row,col))
 
+class Ball(Player):
+     def __init__(self):
+        Sprite.__init__(self)
+        p =pygame.image.load("ball.png")
+        self.image = pygame.transform.scale(p, (40,40))
+        self.rect = self.image.get_rect(center=(-20,-20))
 
-
+     def hit(self, target):
+        return self.rect.colliderect(target)
 
 
 class Bullet(Ball):
 
     def __init__(self, xpos, ypos, hspeed, vspeed):
         super(Bullet, self).__init__()
-        self.image = image.load('bullet.png')
+        icicle = image.load('bullet.png')
+        self.image = pygame.transform.scale(icicle, (20,60))
         self.rect = self.image.get_rect()
         self.rect.x = xpos
         self.rect.y = ypos
@@ -79,7 +74,7 @@ class Bullet(Ball):
         self.rect.x += self.hspeed
         self.rect.y += self.vspeed
         if self.collide():
-            self.kill()
+            self.move(900, randint(0,700))
 
     def collide(self):
         if self.rect.x < 0 - B_WIDTH or self.rect.x > WIDTH:
@@ -88,18 +83,69 @@ class Bullet(Ball):
             return True
 
     def set_direction(self):
-        if self.hspeed > 0:
-            self.image = pygame.transform.rotate(self.image, 270)
-        elif self.hspeed < 0:
-            self.image = pygame.transform.rotate(self.image, 90)
-        elif self.vspeed > 0:
-            self.image = pygame.transform.rotate(self.image, 180)
-
+        self.image = pygame.transform.rotate(self.image, 270)
+        
 def random_bullet(speed):
     
     return Bullet(WIDTH, randint(0, HEIGHT), -speed, 0)
-      
-#######################################################################################
+
+def draw_background(background_img):
+    background_rect = background_img.get_rect()
+    background_rect_width = background_rect.width
+    background_rect_height = background_rect.height
+    for i in range(math.ceil(WIDTH / background_rect.width)):
+        for j in range(math.ceil(HEIGHT / background_rect.height)):
+            screen.blit(background_img, Rect(i * background_rect_width,
+                                             j * background_rect_height,
+                                             background_rect_width,
+                                             background_rect_height))
+
+
+def draw_ice():
+    ice = []
+    rows = range(300,850,300)
+    cols = range(100,650,300)
+    for i in rows:
+      for j in cols:
+        ice.append(Ice(i, j))
+    return ice
+
+def win(hits):
+    while True:
+        draw_background(background_img)
+        t = font.Font(None, 150).render("You Win! ", False, (100,0,255))
+        screen.blit(t, (200, 200))
+
+        t = f.render("Final Score: " + str(hits), False, (255,255,255))
+        screen.blit(t, (320, 0))
+        t = f.render("Press q to quit", False, (255,255,255))
+        screen.blit(t, (400, 400))
+
+        pygame.display.update()
+        e = event.poll()
+        if e.type == KEYDOWN: 
+            if e.key == K_q:
+                quit()
+        
+
+def lose(hits):
+    while True:
+        draw_background(background_img)
+        t = font.Font(None, 150).render("You lost ", False, (255,0,0))
+        screen.blit(t, (200, 200))
+
+        t = f.render("Final Score: " + str(hits), False, (255,255,255))
+        screen.blit(t, (320, 0))
+        t = f.render("Press q to quit", False, (255,255,255))
+        screen.blit(t, (400, 400))
+
+        pygame.display.update()
+        e = event.poll()
+        if e.type == KEYDOWN: 
+            if e.key == K_q:
+                quit()    
+
+#################################### Main ###################################################
 init()
 
 
@@ -107,54 +153,44 @@ WIDTH=900
 HEIGHT=700
 screen = display.set_mode((WIDTH, HEIGHT))
 display.set_caption('Climb through the Ice')
-
-# hide the mouse cursor so we only see shovel
-mouse.set_visible(False)
-
-
-f = font.Font(None, 25)
+background_img = background_img = pygame.image.load('back.png')
+f = font.Font(None, 40)
 DELAY = 3000;
-##### Constructors ######
+
+################ Constructors #################
+
+screen_rect = screen.get_rect()
+ice=draw_ice()
 player = Player()
 ball = Ball()
-ice = []
 gold=Gold()
-
 bullets =[]
 bullet = random_bullet(randint(10,15))
 bullets.append(bullet)
 
 
-
-screen_rect = screen.get_rect()
-rows = range(300,850,100)
-cols = range(100,650,100)
-for i in rows:
-    for j in cols:
-        ice.append(Ice(i, j))
-        
-
-
-# creates a group of sprites so all can be updated at once aka every time i mention sprites, i'm talking about gold and shovel. 
 sprites = RenderPlain(player, ice, ball, gold, bullets)
+
+
+
 
 x_pos=30
 y_pos=30
 repeat=True
 hits=0
 time.set_timer(USEREVENT + 1, DELAY)
-# loop until user quits
+
+
+
+#################### Game Loop ###########################
 while True:
     
     e = event.poll()
-    
-
     if e.type == USEREVENT + 1: # TIME has passed
         gold.move()
     elif e.type == KEYDOWN: 
         if e.key == K_q:
             quit()
-      
         if e.key == K_UP:
             y_pos -= 20
         if e.key == K_DOWN:
@@ -162,61 +198,57 @@ while True:
             
         player.move(x_pos, y_pos)
         
-       
         if e.key == K_SPACE: 
-            x = x_pos
-           
-            while repeat == True:
-                 
+            x = x_pos   
+            
+            while repeat == True:   
+                draw_background(background_img)
+
                 ball.move(x, y_pos)
                 sprites.update()
                 sprites.draw(screen)
                 pygame.display.update()
                 screen.fill(bgcolor)
                 x+=20
+                
                 if ball.hit(gold):
                     mixer.Sound("cha-ching.wav").play()
                     hits +=3
                     gold.move()
                     t = f.render("Score: " + str(hits), False, (255,255,255))
                     screen.blit(t, (320, 0))
+
                 if x > screen_rect.right:
                     mixer.Sound("splash.wav").play()
                     ball.move(-15,-15)
                     break
 
                 for i in ice:
-                    if ball.hit(i):
-                        
+                    if ball.hit(i):  
                         repeat = False
                         ice.remove(i)
-                        
                         i.kill()
                         ball.move(-15,-15)
-                        #i.move(0,0)
                         mixer.Sound("hit.wav").play()
                         hits+=1
-                        
                         break
                 
-                    
-            
             repeat = True
             time.set_timer(USEREVENT + 1, DELAY)
 
     for bullet in bullets:           
         if bullet.hit(player):
+            mixer.Sound("bullet.wav").play()
             bullet.kill()
             bullet.move(-20,-20)
-            print('oops')
+            lose(hits)
+            
 
     if len(ice)==0:
-        break
+        win(hits)
 
-    # refill background color so that we can paint sprites in new locations
-           # draw text to screen.  Can you move it?
-    screen.fill(bgcolor)
-
+    # refill background 
+    draw_background(background_img)
     t = f.render("Score: " + str(hits), False, (255,255,255))
     screen.blit(t, (320, 0))
     # update and redraw sprites
@@ -224,21 +256,4 @@ while True:
     sprites.draw(screen)
     pygame.display.update()
 
-while True:
-    t = font.Font(None, 150).render("You Win! ", False, (255,0,0))
-    screen.blit(t, (200, 200))
-
-    t = f.render("Final Score: " + str(hits), False, (255,255,255))
-    screen.blit(t, (320, 0))
-    t = f.render("Press q to quit", False, (255,255,255))
-    screen.blit(t, (400, 400))
-
-    pygame.display.update()
-    e = event.poll()
-    if e.type == KEYDOWN: 
-        if e.key == K_q:
-            quit()
-    
-
-#thursday morning
 
